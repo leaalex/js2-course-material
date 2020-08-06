3a90a53aec17ea91d861aade7369f58121890dbc
lea_alex <@leX^88#>
2020-08-06 17:45:13 +0500

    023_event

--- 03_event/script.js
+++ 03_event/script.js
@@ -25,93 +25,52 @@ function create(tag, props, ...children) {
             })
         }
     }
-    if (children) {
-        element.append(...children)
-        // children = children.forEach(
-        //     item => (typeof item === 'string')? element.append(children) : item.outerHTML
-        // )
+    if (children) element.append(...children)
 
-    }
-
-
-
-
-    // if (props.classList) element.classList.add(props.classList)
     return element
 }
 
-props = {
-    id: '',
-    className: '',
-    classList: ['',''],
-    attributes: {},
-    properties: {},
-    style: {},
-    data: {},
-}
 
+const input = create(
+    'input',
+    {
+        attributes: {type:'text'},
+        classList: ['form-control']
+    })
 
-
-
-
-
-const container = document.querySelector('.container')
+const button = create(
+    'button',
+    {
+        id:'button-addon2',
+        classList: ['btn', 'btn-outline-secondary'],
+        attributes: {type:'button'},
+    },
+    'Добавить заметку'
+)
+const divBtnContaibner = create('div', {
+    classList: ['input-group-append']
+}, button)
 
 const div = create(
     'div',
     {
         id: 'id_div_wer3d2232dweds',
         className: 'input-group mb-8 mt-3',
-        data: {x:1, y:2, z:78},
-        attributes: {type: 'text'},
-        properties: {hidden: false},
-        style: {borderColor: 'gold'}
-    }
+    },
+    input,
+    divBtnContaibner
 )
-const em = create('button', {style: {color: 'green', display: 'inline-block'}},', этот чертов')
-em.addEventListener('click', function (){ console.log(this) })
 
-const p = create(
-    'p',
-    null,
-    create('em', {style: {color: 'red'}}, 'Привет'),
-    em,
-    'мир!', 'Я Алексей!' )
-// div.classList.add('input-group', 'mb-3')
+const container = document.querySelector('.container')
+container.append(div)
 
-const input = document.createElement('input')
-input.type = 'text'
-input.classList.add('form-control')
-
-const divBtnContaibner = document.createElement('div')
-divBtnContaibner.classList.add('input-group-append')
-
-const button = document.createElement('button')
-button.classList.add('btn', 'btn-outline-secondary')
-button.type = 'button'
-button.id = 'button-addon2'
-button.innerText = 'Button'
-button.onclick = () => {input.value = ''}
-
-
-const button2 = button.cloneNode(true)
-button2.innerText = 'Button 2'
-button2.onclick = function(){
-    this.parentNode.previousElementSibling.value = 'кнопка нажата!'
-}
-
-
-
-const action = () => {
-    button.innerText = button.innerText + 'Нажата'
-    button2.onclick = ''
-}
-
-setTimeout(()=>{ button.removeEventListener('click', action)},3000)
-
-button.addEventListener('click', action)
-
-div.append(input, divBtnContaibner)
-divBtnContaibner.append(button, button2)
-container.append(div, p)
-
+button.addEventListener('click', ()=>{
+    container.append(create('p', {}, input.value))
+    input.value =''
+})
+input.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        container.append(create('p', {}, input.value))
+        input.value =''
+    }
+})
